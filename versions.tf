0860912ae02561479ea77dc526e154adf4fb098d
Jason McVetta <jmcvetta@hey.com>
2023-10-19 17:55:45 -0400

    aws provider 5

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    #    awsutils = {
+    #      source  = "cloudposse/awsutils"
+    #      version = ">= 0.16.0"
+    #    }
+  }
+}
