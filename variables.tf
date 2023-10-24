3eee301eb93301fdff3dcfa8df9a42c8567c5704
Jason McVetta <jmcvetta@hey.com>
2023-10-24 18:09:43 -0400

    vars are not nullable

--- variables.tf
+++ variables.tf
@@ -1,20 +1,24 @@
 variable "name" {
   type        = string
   description = "Unique name (no spaces)"
+  nullable    = false
 }
 
 variable "tags" {
   type        = map(string)
   description = "AWS Tags"
+  nullable    = false
   default     = {}
 }
 
 variable "subnet_ids" {
   type        = list(string)
   description = "Subnets where target databases live and where Lambda function will be deployed"
+  nullable    = false
 }
 
 variable "security_group_ids" {
   type        = list(string)
   description = "Security groups granting access to target databases"
+  nullable    = false
 }
