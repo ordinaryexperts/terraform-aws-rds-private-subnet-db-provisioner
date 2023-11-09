0c88026d4d83d0dbfe2e0ab558573e0b1cf6fd9b
Jason McVetta <jmcvetta@hey.com>
2023-11-09 16:36:19 -0500

    probably not using this either

--- artifact.tf
+++ artifact.tf
@@ -1,7 +1,12 @@
 /*
-data "archive_file" "lambda_function" {
-  type        = "zip"
-  source_dir  = "${path.module}/function"
-  output_path = "${path.module}/lambda_function.zip"
+module "external_module_artifact" {
+  source  = "cloudposse/module-artifact/external"
+  version = "0.8.0"
+
+  filename    = "test.zip"
+  git_ref     = "example"
+  module_name = "terraform-external-module-artifact"
+  module_path = path.module
+
 }
 */
